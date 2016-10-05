64fc5bcbc5deb5a0b99d3a483f34cb93d3692460
Yellow5A5 <scnu.hww@foxmail.com>
2016-10-06 00:48:04 +0800

    add: Constants.

---
+++ library/src/main/java/yellow5a5/nougatnetworktool/Constants.java
@@ -0,0 +1,63 @@
+package yellow5a5.nougatnetworktool;
+
+/**
+ * Created by Yellow5A5 on 16/10/4.
+ */
+
+public class Constants {
+
+    public static final String NETWORK_CONNECTIVITY_CHANGE = "android.net.conn.CONNECTIVITY_CHANGE";
+
+    public static final String TYPE_WIFI = "WIFI";
+
+    public static final String TYPE_MOBILE = "MOBILE";
+
+    /** Network type is unknown */
+    public static final int NETWORK_TYPE_UNKNOWN = 0;
+    /** Current network is GPRS */
+    public static final int NETWORK_TYPE_GPRS = 1;
+    /** Current network is EDGE */
+    public static final int NETWORK_TYPE_EDGE = 2;
+    /** Current network is UMTS */
+    public static final int NETWORK_TYPE_UMTS = 3;
+    /** Current network is CDMA: Either IS95A or IS95B*/
+    public static final int NETWORK_TYPE_CDMA = 4;
+    /** Current network is EVDO revision 0*/
+    public static final int NETWORK_TYPE_EVDO_0 = 5;
+    /** Current network is EVDO revision A*/
+    public static final int NETWORK_TYPE_EVDO_A = 6;
+    /** Current network is 1xRTT*/
+    public static final int NETWORK_TYPE_1xRTT = 7;
+    /** Current network is HSDPA */
+    public static final int NETWORK_TYPE_HSDPA = 8;
+    /** Current network is HSUPA */
+    public static final int NETWORK_TYPE_HSUPA = 9;
+    /** Current network is HSPA */
+    public static final int NETWORK_TYPE_HSPA = 10;
+    /** Current network is iDen */
+    public static final int NETWORK_TYPE_IDEN = 11;
+    /** Current network is EVDO revision B*/
+    public static final int NETWORK_TYPE_EVDO_B = 12;
+    /** Current network is LTE */
+    public static final int NETWORK_TYPE_LTE = 13;
+    /** Current network is eHRPD */
+    public static final int NETWORK_TYPE_EHRPD = 14;
+    /** Current network is HSPA+ */
+    public static final int NETWORK_TYPE_HSPAP = 15;
+    /** Current network is GSM  */
+    public static final int NETWORK_TYPE_GSM = 16;
+    /** Current network is TD_SCDMA  */
+    public static final int NETWORK_TYPE_TD_SCDMA = 17;
+    /** Current network is IWLAN  */
+    public static final int NETWORK_TYPE_IWLAN = 18;
+
+
+    public static final int NETWORK_CLASS_UNKNOWN = 0;
+    /** Class of broadly defined "2G" networks.  */
+    public static final int NETWORK_CLASS_2_G = 1;
+    /** Class of broadly defined "3G" networks.  */
+    public static final int NETWORK_CLASS_3_G = 2;
+    /** Class of broadly defined "4G" networks.  */
+    public static final int NETWORK_CLASS_4_G = 3;
+    public static final int NETWORK_CLASS_WIFI = 100;
+}
