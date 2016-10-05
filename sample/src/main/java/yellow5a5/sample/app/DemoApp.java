f4ec5cc6b8a75d03b1d029c132743a226f214ce8
Yellow5A5 <scnu.hww@foxmail.com>
2016-10-05 10:39:59 +0800

    Init Project.

---
+++ sample/src/main/java/yellow5a5/sample/app/DemoApp.java
@@ -0,0 +1,16 @@
+package yellow5a5.sample.app;
+
+import android.app.Application;
+
+/**
+ * Created by Yellow5A5 on 16/10/4.
+ */
+
+public class DemoApp extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
